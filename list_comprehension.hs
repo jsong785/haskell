a3eb02f6c38399d0c1bae8699a2e4a71824f6662
Juhwan Song <jsong785@gmail.com>
2020-05-02 22:37:13 -0600

    adding guards + pattern match mix

--- list_comprehension.hs
+++ list_comprehension.hs
@@ -1,7 +1,8 @@
 import Data.List
 
 ifEvenAdd :: [(Int, Int)] -> [Int]
-ifEvenAdd [] = [1, 2, 3, 4, 5]
-ifEvenAdd [(1, 2)] = [1, 2, 22222222222222222]
-ifEvenAdd xs = [a + b | (a, b) <- xs, even(a), even(b)]
+ifEvenAdd l
+    | length l == 0 = [1, 2, 3 , 4]
+    | [(1, 2)] <- l = [1, 2, 22222222222222222]
+    | otherwise <- l = [a + b | (a, b) <- l, even(a), even(b)]
 
