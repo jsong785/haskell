73043f25ca92791aba493673e629732c6082a5b6
Juhwan Song <jsong785@gmail.com>
2020-05-05 00:24:11 -0600

    changes

--- list_comprehension.hs
+++ list_comprehension.hs
@@ -4,3 +4,25 @@ ifEvenAdd l
     | [(1, 2)] <- l = [1, 2, 22222222222222222]
     | otherwise <- l = [a + b | (a, b) <- l, even(a), even(b)]
 
+customTake :: (Num i, Ord i) => i -> [a] -> [a]
+customTake n _
+    | n <= 0 = []
+customTake _ [] = []
+customTake n (x:xs) = x : customTake (n-1) xs
+
+customReverse :: [a] -> [a]
+customReverse [] = []
+customReverse (x:xs) = customReverse xs ++ [x]
+
+customSequence :: (Num a) => a -> Int -> [a]
+customSequence _ 0 = []
+customSequence init 1 = [init]
+customSequence init count = [init] ++ customSequence (init+1) (count-1)
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) =
+    let lessSort = quickSort (filter (<=x) xs)
+        bigSort = quickSort (filter (>x) xs)
+    in lessSort ++ [x] ++ bigSort
+
